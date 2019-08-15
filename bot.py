b99d1b5d3fb5e2ba9657cf4d6969dd0945b21670
pratik2799 <pithore.pratik@gmail.com>
2019-08-15 23:53:16 +0530

    Hide the Token
    using enviroment variables helps to hide the passwords and secret keys
    
    Signed-off-by: pratik2799 <pithore.pratik@gmail.com>

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
 from telegram.ext import Updater, CommandHandler
+import os
 
 def start(bot, update):
     chat_id = update.message.chat_id
     bot.send_message(chat_id=chat_id, text="hello")
 
 def main ():
-    updater = Updater('TOKEN')
+    updater = Updater(os.environ.get('TOKEN'))
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('start',start))
     updater.start_polling()
