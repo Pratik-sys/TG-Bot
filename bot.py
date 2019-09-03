3af95300aa78118a5e18afbbe43fb03cd5448709
pratik2799 <pithore.pratik@gmail.com>
2019-09-03 17:14:59 +0530

    Enable logging
    Signed-off-by: pratik2799 <pithore.pratik@gmail.com>

--- bot.py
+++ bot.py
@@ -1,5 +1,13 @@
 from telegram.ext import Updater, CommandHandler
 import os
+import logging
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
 
 def start(bot, update):
     chat_id = update.message.chat_id
