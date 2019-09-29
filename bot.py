8b29cd300dd77279d5adf011e36d746c2c2fa4b1
Pratik-sys <pithore.pratik@gmail.com>
2019-09-29 23:03:21 +0530

    logger: get the info
    Signed-off-by: Pratik-sys <pithore.pratik@gmail.com>

--- bot.py
+++ bot.py
@@ -14,7 +14,6 @@ def start(update, context):
     """Send a message when the command /start is issued."""
     update.message.reply_text('Hi!')
 
-
 def help(update, context):
     """Send a message when the command /help is issued."""
     update.message.reply_text('Help!')
@@ -48,4 +47,5 @@ def main():
 
 
 if __name__ == '__main__':
+    logger.info("Starting Bot!!")
     main()
